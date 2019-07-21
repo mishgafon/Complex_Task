d93e1f27846e3853adf25f568a61880c0ac061f4
mishgafon <52297389+mishgafon@users.noreply.github.com>
2019-07-21 21:41:01 +0400

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -1,50 +1,37 @@
+let date = new Date();
+	 day = date.getDay();
+	 hours = date.getHours();
 
-window.addEventListener('DOMContentLoaded', function(){
-	'use strict';
 
-	//Timer
-function countTimer(deadline){
-	let timerHours = document.querySelector('#timer-hours'),
-		timerMinutes = document.querySelector('#timer-minutes'),
-		timerSeconds = document.querySelector('#timer-seconds');
-		
-
-		function getTimeRemaining(){
-			let	dateStop = new Date(deadline).getTime(),
-				dateNow = new Date().getTime(),
-				timeRemaining = (dateStop - dateNow) / 1000,
-				seconds = Math.floor(timeRemaining % 60),
-				minutes = Math.floor((timeRemaining / 60) % 60),
-				hours = Math.floor(timeRemaining / 60 / 60);
-	
-				return {timeRemaining, hours, minutes, seconds};
-				
+function hello() {
+	if (hours < 12) {
+		return ('Доброе утро!');
+	} else 
+		if (hours > 12 &&  hours < 18 ){
+			return ('Добрый день!');
+		}else 
+			if (hours > 18 &&  hours < 24 ){
+				return ('Добрый вечер!');
+			}
 		};
-		
-		
-		function updateClock(){
-			let timer = getTimeRemaining();
-			
+document.write(hello());
+document.write('<br>');
 
-			if(timer.timeRemaining > 0){
-
-				timerHours.textContent = (`0` + timer.hours).slice(-2);
-				timerMinutes.textContent = (`0` + timer.minutes).slice(-2); 
-				timerSeconds.textContent = (`0` + timer.seconds).slice(-2); 
-			 } else {
-				timerHours.textContent = '00';
-				timerMinutes.textContent = '00';
-				timerSeconds.textContent = '00';
-				
-			 }
-			 setInterval(updateClock, 1000);
-		};
+	  
+function showDay(day) {
+	let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+	return days[day];
+};
+document.write('Сегодня: '+ showDay(day));
+document.write('<br>');
+document.write('Текущее время: ' + date.toLocaleTimeString());
+document.write('<br>');
 
 
-	updateClock();
 
-	}
+let  newYear = new Date('January 01, 2020');
+	 msPerDay = 24 * 60 * 60 * 1000;
+	 	result = Math.round((newYear.getTime() - date.getTime()) /msPerDay );
+document.write(`До нового года осталось ${result} дня(дней)`);
 
-countTimer('25 july 2019');
 
-});
